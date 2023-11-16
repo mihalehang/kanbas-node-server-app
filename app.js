66e1761f9c1aa6f9e7a7b7eed1ee205dd5ec20ec
Michael Huang <michael.s.huang.01@gmail.com>
2023-11-15 22:42:33 -0500

    modules

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from './modules/routes.js';
 
 const app = express();
 app.use(cors());
@@ -10,4 +11,5 @@ app.use(express.json());
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
+ModuleRoutes(app);
 app.listen(4000);
