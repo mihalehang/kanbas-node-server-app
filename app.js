76c2c8651ba0c6a29913f9f771bb0846725ccfa4
Michael Huang <michael.s.huang.01@gmail.com>
2023-11-21 13:21:31 -0500

    ready for remote deploy

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-import express from 'express';
+import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from './modules/routes.js';
+import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
-// import session from "express-session";
+import session from "express-session";
 
 import mongoose from "mongoose";
-import UserRoutes from './users/routes.js';
+import UserRoutes from "./users/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 Hello(app);
