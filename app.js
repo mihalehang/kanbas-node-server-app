4c933d866fefe6a4ddbcf35014fcc2af155008e1
Michael Huang <michael.s.huang.01@gmail.com>
2023-11-20 22:36:23 -0500

    up to multiple sessions

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ import ModuleRoutes from './modules/routes.js';
 import "dotenv/config";
 // import session from "express-session";
 
+import mongoose from "mongoose";
+import UserRoutes from './users/routes.js';
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
